b4eda1a57259b991fa76eaa223b07b8eec9b58c9
rschmutz-orif <raphael.schmutz@orif.ch>
2026-05-22 14:38:16 +0000

    Fix - Missing files

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,1 @@
-const header = document.createElement("h1");
-const helloWorld = document.createTextNode("Hello World! TESTING");
 
-header.appendChild(helloWorld);
-document.body.appendChild(header);
